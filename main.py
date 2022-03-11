3e335793f5331ea29b3e403f62f2e05f3248722a
parshan77 <parshanteimouri@yahoo.com>
2022-03-11 14:39:56 +0330

    Implementing subtract function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+def add(num1, num2):
+      return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
 print("Please select operation -\n" \
       "1. Add\n" \
-      "2. Subtract\n" \
-      "3. Multiply\n" \
-      "4. Divide\n")
+@@ -16,4 +18,7 @@ def add(num1, num2):
 
-# Take input from the user
-select = int(input("Select operations form 1, 2, 3, 4 :"))
-
-number_1 = int(input("Enter first number: "))
-number_2 = int(input("Enter second number: "))
+if select == 1:
+      print(number_1, "+", number_2, "=",
+            add(number_1, number_2)) 
+            add(number_1, number_2))
+elif select == 2:
+      print(number_1, "-", number_2, "=",
+            subtract(number_1, number_2)) 
