bb9d8f6ad99b7feb2b14a74f52f771a9b1a99e8a
parshan77 <parshanteimouri@yahoo.com>
2022-03-12 16:06:51 +0330

    getting input handled

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ print("Please select operation -\n" \
 
 # Take input from the user
 select = int(input("Select operations form 1, 2, 3, 4 :"))
+number_1, number_2 = int(input()), int(input())
 
 if select == 1:
 	print(number_1, "+", number_2, "=",
