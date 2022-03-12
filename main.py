c7d3e4e0d1974816382249bdb1a3a710b610ff67
parshan77 <parshanteimouri@yahoo.com>
2022-03-12 16:00:33 +0330

    mult and divide implemented

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def subtract(num1, num2):
 def divide(num1, num2):
 	return num1 / num2
 
-
 print("Please select operation -\n" \
       "1. Add\n" \
       "2. Subtract\n" \
@@ -26,3 +25,9 @@ if select == 1:
 elif select == 2:
 	print(number_1, "-", number_2, "=",
 	      subtract(number_1, number_2))
+elif select == 3:
+	print(number_1, "/", number_2, "=",
+	      divide(number_1, number_2))
+elif select == 3:
+	print(number_1, "/", number_2, "=",
+	      mult(number_1, number_2))
