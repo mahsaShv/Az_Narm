f085c732e6fa8998d4cd34b75238d3436acfe923
parshan77 <parshanteimouri@yahoo.com>
2022-03-12 15:57:21 +0330

    divide implemented, minor refactor

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def add(num1, num2):
-      return num1 + num2
+	return num1 + num2
+
 
 def subtract(num1, num2):
-    return num1 - num2
+	return num1 - num2
+
+
+def divide(num1, num2):
+	return num1 / num2
+
 
 print("Please select operation -\n" \
       "1. Add\n" \
@@ -14,9 +20,9 @@ print("Please select operation -\n" \
 select = int(input("Select operations form 1, 2, 3, 4 :"))
 
 if select == 1:
-      print(number_1, "+", number_2, "=",
-            add(number_1, number_2))
+	print(number_1, "+", number_2, "=",
+	      add(number_1, number_2))
 
 elif select == 2:
-      print(number_1, "-", number_2, "=",
-            subtract(number_1, number_2))
+	print(number_1, "-", number_2, "=",
+	      subtract(number_1, number_2))
